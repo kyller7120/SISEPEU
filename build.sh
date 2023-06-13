07e10e0f9cb9f96d0517164e4975ec960a3fd118
Marlon Rivera <131151048+kyller7120@users.noreply.github.com>
2023-06-12 18:27:49 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o errexit
 python -m pip install -r ./requirements.txt
 
 python manage.py collectstatic --no-input
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'covik19')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('grupo28', 'grupo28', 'covik19')" | python manage.py shell
 python manage.py migrate
