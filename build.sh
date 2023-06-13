46344eb3e0806539fea21ce8f27d7d5b70eb94b5
Marlon Rivera <131151048+kyller7120@users.noreply.github.com>
2023-06-12 18:16:37 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ set -o errexit
 python -m pip install -r ./requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').delete()"
+
 python manage.py migrate
